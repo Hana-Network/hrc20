4529bf4b0df47b10fc4e4d267dfeb5889b83fe3f
satellitex <s.a.t.e.3.ths@gmail.com>
2023-11-22 14:21:45 +0400

    Deploy scripts を変更 (#2)
    * add hanaAlpha network config
    
    * fix README.md
    
    * change deploy script
    
    * rm template contract
    
    ---------
    
    Co-authored-by: peaceandwhisky <pioneerandf@gmail.com>

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,19 +4,15 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const hanaBTC = await ethers.deployContract("HRC20", ["hanaBTC", "hBTC"]);
+  const hanaETH = await ethers.deployContract("HRC20", ["hanaETH", "hETH"]);
 
-  await lock.waitForDeployment();
+  await hanaBTC.waitForDeployment();
+  await hanaETH.waitForDeployment();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`hanaBTC deployed to ${hanaBTC.target}`);
+  console.log(`hanaETH deployed to ${hanaETH.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
