c7449ccc8cae0f4d6483b5fca947bde2b96c88e9
wildmouse <wildmouse0813@gmail.com>
2023-12-07 19:16:44 +0900

    Add hardhat watcher

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-watcher";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -21,8 +22,17 @@ const config: HardhatUserConfig = {
     hanaAlpha: {
       url: process.env.HANA_ALPHA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
-    }
-  }
+    },
+  },
+  watcher: {
+    test: {
+      tasks: [{ command: "test", params: { testFiles: ["{path}"] } }],
+      files: ["./test/**/*"],
+      verbose: true,
+      clearOnStart: true,
+      start: "echo Running my test task now..",
+    },
+  },
 };
 
 export default config;
